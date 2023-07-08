19cdc7a6d691dfd7c2893dd0463d5d95806db1dd
Putinabillaa <aidiraputiabil@gmail.com>
2023-07-09 00:05:00 +0800

    fix clustering

--- src/Clustering.py
+++ src/Clustering.py
@@ -1,26 +1,17 @@
 from Kruskal import *
-from collections import defaultdict
+import networkx as nx
 
 def clustering(graph, k):
-    parent = {}
-    clusters = []
-    
-    for node in graph.nodes():
-        parent[node] = node
+    ''' Return the clusters of a graph after removing k edges'''
+    graph_copy = graph.copy()
     try:
-        mst_edges, _ = kruskal(graph)
+        mst_edges, _ = kruskal(graph_copy)
+        print(mst_edges)
     except ValueError:
         raise ValueError
-    
-    for i in range(len(mst_edges) - k + 1):
-        u, v = mst_edges[i]
-        union(parent, u, v)
-    
-    for node in graph.nodes():
-        clusters.append(find(parent, node))
-    cluster_dict = defaultdict(list)
-    
-    for i, cluster in enumerate(clusters):
-        cluster_dict[cluster].append(i)
-    
-    return list(cluster_dict.values())
+    for edge in graph_copy.edges():
+        if(edge not in mst_edges):
+            graph_copy.remove_edge(edge[0], edge[1])
+    for i in range(len(mst_edges) - 1, len(mst_edges) - k, -1):
+        graph_copy.remove_edge(mst_edges[i][0], mst_edges[i][1])
+    return list(nx.connected_components(graph_copy))
